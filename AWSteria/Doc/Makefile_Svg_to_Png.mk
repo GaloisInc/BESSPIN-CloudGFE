bb9d36377b1cd8de54e87eca788c1df0d71b4680
Rishiyur Nikhil <nikhil@acm.org>
2020-12-14 18:10:35 -0500

    Some tweaks to the Doc directories in AWSteria and AWSteria_vanilla

--- AWSteria/Doc/Makefile_Svg_to_Png.mk
+++ AWSteria/Doc/Makefile_Svg_to_Png.mk
@@ -1,14 +1,12 @@
-.PHONY: all_svg_to_png
-all_svg_to_png:
-	make  -f Makefile_Svg_to_Png.mk  FIG=Fig_1_Build_Run_AWS_Example  svg_to_png
-	make  -f Makefile_Svg_to_Png.mk  FIG=Fig_2_AWS_BSV_XSim_Test      svg_to_png
-	make  -f Makefile_Svg_to_Png.mk  FIG=Fig_3_AWS_BSV_XSim_WindSoC   svg_to_png
-	make  -f Makefile_Svg_to_Png.mk  FIG=Fig_3_Detail                 svg_to_png
-	make  -f Makefile_Svg_to_Png.mk  FIG=Fig_4_Debugger               svg_to_png
+Figs = \
+	Fig_1_Build_Run_AWS_Example.png \
+	Fig_2_AWS_BSV_XSim_Test.png \
+	Fig_3_AWS_BSV_XSim_WindSoC.png \
+	Fig_3_Detail.png \
+	Fig_4_Debugger.png \
 
-.PHONY: svg_to_png
-svg_to_png:
-	inkscape --file=$(FIG).svg \
-		--export-png=$(FIG).png \
-		--export-area-drawing \
-		-d 300 -y 0.0
+
+all: $(Figs)
+
+$(Figs): %.png: %.svg
+	inkscape --export-filename=$@  --export-area-drawing  -d 300 -y 0.0 $<
