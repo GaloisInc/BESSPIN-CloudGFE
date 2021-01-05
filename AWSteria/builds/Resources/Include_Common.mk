cae393802cbe5806a63ae39e97ffcec5ac8f0278
Jessica Clarke <jrtc27@jrtc27.com>
2021-01-05 02:29:57 +0000

    Drop Flute Testbench SoC/Top directories from search path
    Previously the SoC directory was needed for UART_Model but that is now
    present in AWSteria itself. Including these directories is dangerous as
    the SoC and platform are very different between the Flute testbench and
    AWSteria, so don't.

--- AWSteria/builds/Resources/Include_Common.mk
+++ AWSteria/builds/Resources/Include_Common.mk
@@ -57,11 +57,10 @@ SRC_CORE ?= $(REPO)/src_Core/Core_v2
 
 CORE_DIRS = $(REPO)/src_Core/CPU:$(REPO)/src_Core/ISA:$(REPO)/src_Core/RegFiles:$(SRC_CORE):$(REPO)/src_Core/Cache_Config:$(REPO)/src_Core/$(NEAR_MEM_VM_DIR):$(REPO)/src_Core/PLIC:$(REPO)/src_Core/Near_Mem_IO:$(REPO)/src_Core/Debug_Module:$(REPO)/src_Core/BSV_Additional_Libs
 
-AXI4_DIRS          = $(REPO)/src_Testbench/Fabrics/AXI4:$(REPO)/src_Testbench/Fabrics/AXI4_Lite
-TESTBENCH_DIRS     = $(REPO)/src_Testbench/Top:$(REPO)/src_Testbench/SoC
-AWS_TESTBENCH_DIRS = $(AWSTERIA)/src_Testbench_AWS/Top:$(AWSTERIA)/src_Testbench_AWS/SoC
+AXI4_DIRS      = $(REPO)/src_Testbench/Fabrics/AXI4:$(REPO)/src_Testbench/Fabrics/AXI4_Lite
+TESTBENCH_DIRS = $(AWSTERIA)/src_Testbench_AWS/Top:$(AWSTERIA)/src_Testbench_AWS/SoC
 
-BSC_PATH = $(CUSTOM_DIRS):$(CORE_DIRS):$(AXI4_DIRS):$(AWS_TESTBENCH_DIRS):$(TESTBENCH_DIRS):+
+BSC_PATH = $(CUSTOM_DIRS):$(CORE_DIRS):$(AXI4_DIRS):$(TESTBENCH_DIRS):+
 
 # ----------------
 # Top-level file and module
